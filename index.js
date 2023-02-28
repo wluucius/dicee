8cda3d3059b8783e215666115bb9ddd629f26448
Glazos <frantisek.tomas02@gmail.com>
2023-02-28 18:59:30 +0100

    add dicee file

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg = "dice" + randomNumber1 + ".png";
+
+var randomImgSource = "images/" + randomDiceImg;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImgSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 win 🤩";
+} else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 win 🤩";
+} else {
+  document.querySelector("h1").innerHTML = "tie 🥱";
+}
